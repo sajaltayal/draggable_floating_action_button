26dbf250800ee257349f6dacfefee740b0a5a3a1
sajaltayal <73775153+sajaltayal@users.noreply.github.com>
2021-07-28 14:28:02 +0530

    Update draggable_floating_action_button.dart

--- lib/draggable_floating_action_button.dart
+++ lib/draggable_floating_action_button.dart
@@ -1,142 +1,162 @@
-library draggable_floating_action_button;
+library draggable_floating_button;
 
 import 'package:flutter/material.dart';
 
-class DraggableFloatingActionButton extends StatefulWidget {
-  final Function onPressed;
-  final Icon icon;
+const BoxConstraints _kSizeConstraints = BoxConstraints.tightFor(
+  width: 48.0,
+  height: 48.0,
+);
 
-  const DraggableFloatingActionButton({
-    this.onPressed,
-    this.icon,
-  });
+const BoxConstraints _kMiniSizeConstraints = BoxConstraints.tightFor(
+  width: 40.0,
+  height: 40.0,
+);
+
+/*
+const BoxConstraints _kExtendedSizeConstraints = BoxConstraints(
+  minHeight: 48.0,
+  maxHeight: 48.0,
+);
+*/
+
+const Offset _kDefaultOffset = Offset(0, 0);
+
+class _DefaultHeroTag {
+  const _DefaultHeroTag();
+  @override
+  String toString() => '<default FloatingActionButton tag>';
+}
+
+class DraggableFloatingActionButton extends StatefulWidget {
+  DraggableFloatingActionButton({
+    Key key,
+    this.child,
+    this.tooltip,
+    this.foregroundColor,
+    this.backgroundColor,
+    this.heroTag = const _DefaultHeroTag(),
+    this.elevation = 6.0,
+    this.highlightElevation = 12.0,
+    @required this.onPressed,
+    this.mini = false,
+    this.shape = const CircleBorder(),
+    this.clipBehavior = Clip.none,
+    this.materialTapTargetSize,
+    this.isExtended = false,
+    this.appContext,
+    this.appBar,
+    this.data = 'dfab',
+    this.offset = _kDefaultOffset,
+  })  : assert(elevation != null),
+        assert(highlightElevation != null),
+        assert(mini != null),
+        assert(shape != null),
+        assert(isExtended != null),
+        assert(appContext != null),
+        _sizeConstraints = mini ? _kMiniSizeConstraints : _kSizeConstraints,
+        super(key: key);
+
+  final Widget child;
+
+  final String tooltip;
+
+  final Color foregroundColor;
+
+  final Color backgroundColor;
+
+  final Object heroTag;
+
+  final VoidCallback onPressed;
+
+  final double elevation;
+
+  final double highlightElevation;
+
+  final bool mini;
+
+  final ShapeBorder shape;
+
+  final Clip clipBehavior;
+
+  final bool isExtended;
+
+  final MaterialTapTargetSize materialTapTargetSize;
+
+  final BuildContext appContext;
+
+  final AppBar appBar;
+
+  final Offset offset;
+
+  final BoxConstraints _sizeConstraints;
+
+  final String data;
 
   @override
-  _DraggableFloatingActionButtonState createState() =>
-      _DraggableFloatingActionButtonState();
+  State<StatefulWidget> createState() => _DraggableFloatingActionButtonState();
 }
 
 class _DraggableFloatingActionButtonState
     extends State<DraggableFloatingActionButton> {
-  bool isLeftDragging = false;
-  bool isRightDragging = false;
-  bool mainFabRight = true;
+  Offset dynamicOffset;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 68, //standard height of fab plus 16+16 margin above and below
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0), //standard fab padding
-            child: mainFabRight
-                ? DragTarget(
-                    builder: (context, candidateData, rejectedData) {
-                      return isRightDragging
-                          ? Opacity(
-                              opacity: 0.3,
-                              child: FloatingActionButton(
-                                child: widget.icon,
-                                onPressed: null,
-                              ),
-                            )
-                          : null;
-                    },
-                    onWillAccept: (data) => true,
-                    onAccept: (data) {
-                      setState(() {
-                        mainFabRight = false;
-                      });
-                    },
-                  )
-                : LongPressDraggable(
-                    onDragStarted: () {
-                      setState(() {
-                        isLeftDragging = true;
-                      });
-                    },
-                    onDragEnd: (_) {
-                      setState(() {
-                        isLeftDragging = false;
-                      });
-                    },
-                    axis: Axis.horizontal,
-                    child: FloatingActionButton(
-                      child: widget.icon,
-                      onPressed: widget.onPressed,
-                    ),
-                    feedback: FloatingActionButton(
-                      child: widget.icon,
-                      onPressed: null,
-                    ),
-                    childWhenDragging: Opacity(
-                      opacity: 0.1,
-                      child: FloatingActionButton(
-                        child: widget.icon,
-                        onPressed: null,
-                      ),
-                    ),
-                  ),
-          ),
-          Expanded(
-            child: Container(),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: mainFabRight
-                ? LongPressDraggable(
-                    onDragStarted: () {
-                      setState(() {
-                        isRightDragging = true;
-                      });
-                    },
-                    onDragEnd: (_) {
-                      setState(() {
-                        isRightDragging = false;
-                      });
-                    },
-                    axis: Axis.horizontal,
-                    child: FloatingActionButton(
-                      child: widget.icon,
-                      onPressed: widget.onPressed,
-                    ),
-                    feedback: FloatingActionButton(
-                      child: widget.icon,
-                      onPressed: null,
-                    ),
-                    childWhenDragging: Opacity(
-                      opacity: 0.1,
-                      child: FloatingActionButton(
-                        child: widget.icon,
-                        onPressed: null,
-                      ),
-                    ),
-                  )
-                : DragTarget(
-                    builder: (context, candidateData, rejectedData) {
-                      return isLeftDragging
-                          ? Opacity(
-                              opacity: 0.3,
-                              child: FloatingActionButton(
-                                child: widget.icon,
-                                onPressed: null,
-                              ),
-                            )
-                          : null;
-                    },
-                    onWillAccept: (data) => true,
-                    onAccept: (data) {
-                      //make this the new main fab
-                      setState(() {
-                        mainFabRight = true;
-                      });
-                    },
-                  ),
-          ),
-        ],
-      ),
-    );
+    if (dynamicOffset == null) {
+      dynamicOffset = widget.offset;
+    }
+
+    FloatingActionButton _floatingActionButton = new FloatingActionButton(
+        key: widget.key,
+        child: widget.child,
+        tooltip: widget.tooltip,
+        backgroundColor: widget.backgroundColor,
+        foregroundColor: widget.foregroundColor,
+        heroTag: widget.heroTag,
+        elevation: widget.elevation,
+        highlightElevation: widget.highlightElevation,
+        onPressed: widget.onPressed,
+        shape: widget.shape,
+        isExtended: widget.isExtended,
+        materialTapTargetSize: widget.materialTapTargetSize,
+        clipBehavior: widget.clipBehavior,
+        mini: widget.mini);
+
+    return Positioned(
+        left: dynamicOffset.dx,
+        top: dynamicOffset.dy,
+        child: Draggable(
+          data: widget.data,
+          child: _floatingActionButton,
+          feedback: _floatingActionButton,
+          childWhenDragging: Container(),
+          onDraggableCanceled: (Velocity velocity, Offset offset) {
+            setState(() {
+              double appBarHeight = 0;
+              if (widget.appBar != null) {
+                appBarHeight = widget.appBar.preferredSize.height;
+              }
+
+              var dy = offset.dy -
+                  appBarHeight -
+                  MediaQuery.of(widget.appContext).padding.top;
+
+              var maxDy = MediaQuery.of(widget.appContext).size.height -
+                  MediaQuery.of(widget.appContext).padding.bottom -
+                  appBarHeight -
+                  MediaQuery.of(widget.appContext).padding.top;
+
+              if (dy < 0) {
+                dy = 0;
+              } else if (dy > maxDy) {
+                dy = maxDy - widget._sizeConstraints.maxHeight;
+              }
+
+              Offset newOffset = new Offset(offset.dx, dy);
+              dynamicOffset = newOffset;
+            });
+          },
+        ));
   }
 }
+
